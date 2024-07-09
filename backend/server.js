f9d04a548eb692b963d5b33a950d8981d69ffac3
Fitri HY <noreply.orzpartners@gmail.com>
2024-07-09 21:50:35 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.use(cors());
 app.use('/api', require('./routes/Api'));
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.listen(PORT, () => {
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const fs = require('fs');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
     filename: 'bundle.js',
   },
   module: {
@@ -30,10 +32,43 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: 'index.html'
-    })
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { 
+          from: './public/images', to: 'images'
+        },
+        { 
+          from: '../backend', to: 'backend'
+        }
+      ]
+    }),
+    {
+      apply: (compiler) => {
+        compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+          const packageJsonContent = {
+            name: 'backend-app',
+            version: '1.0.0',
+            description: 'Backend application',
+            main: 'server.js',
+            scripts: {
+              start: 'node server.js'
+            },
+            dependencies: {
+              "cors": "^2.8.5",
+              "express": "^4.17.1",
+              "path": "^0.12.7"
+            },
+            license: 'MIT'
+          };
+          const packageJsonPath = path.join(__dirname, '..', 'dist', 'backend', 'package.json');
+          fs.writeFileSync(packageJsonPath, JSON.stringify(packageJsonContent, null, 2));
+        });
+      }
+    }
   ],
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, '../dist'),
     compress: true,
     port: 9000,
     hot: true,
