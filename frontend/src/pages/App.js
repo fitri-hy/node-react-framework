90f26d05e6893a03ea486d7fafc9c3ee837c999b
fitri-hy <noreply.orzpartners@gmail.com>
2024-07-10 09:24:03 +0700

    update

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -12,27 +12,23 @@ const App = () => {
 
   return (
     <main className="app">
-      <div className="flex flex-col w-full min-h-screen justify-center items-center bg-zinc-800">
-        <div className="flex flex-col px-6 py-12 rounded-lg text-white max-w-3xl m-auto">
-          <img className="h-36 w-36 m-auto" src="/images/logo.png" alt="Logo" />
-          <h1 className="text-3xl text-center font-extrabold mt-6 text-yellow-500">
-            {message || 'Loading...'}
-          </h1>
-          <p className="text-center m-auto max-w-lg text-gray-200 mt-4">
-            Fullstack Framework with a backend using Node.js and a frontend using React.js 
-            running on one server combines the power of these two technologies to build 
-            modern and efficient web applications.
-          </p>
-          <div className="flex gap-6 w-full justify-center items-center mt-6">
-            <a className="font-bold text-green-500 hover:text-green-600 underline" href="https://i-as.dev/" target="_blank">
-              Official Site
-            </a>
-            <a className="font-bold text-green-500 hover:text-green-600 underline" href="https://github.com/fitri-hy/fullstack-framework" target="_blank">
-              Github
-            </a>
-          </div>
-        </div>
-      </div>
+      <div class="container">
+		<div class="card">
+		  <img class="logo" src="/images/logo.png" alt="Logo" />
+		  <h1 class="title">
+			{message || 'Loading...'}
+		  </h1>
+		  <p class="description">
+			Fullstack Framework with a backend using Node.js and a frontend using React.js 
+			running on one server combines the power of these two technologies to build 
+			modern and efficient web applications.
+		  </p>
+		  <div class="links">
+			<a class="link" href="https://i-as.dev/" target="_blank">Official Site</a>
+			<a class="link" href="https://github.com/fitri-hy/fullstack-framework" target="_blank">Github</a>
+		  </div>
+		</div>
+	  </div>
     </main>
   );
 };
